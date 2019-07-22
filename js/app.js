6410d54698739a9e936a7fea732898e42895fbf0
Piotr Fidler <fidler.piotr@gmail.com>
2019-07-22 13:19:17 +0200

    add API - at work

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//Mustache template
+function generateTemplate(name, data, basicElement) {
+	var template = document.getElementById(name).innerHTML;
+	var element = document.createElement(basicElement || 'div');
+
+	Mustache.parse(template);
+	element.innerHTML = Mustache.render(template, data);
+
+	return element;
+}
+// OGÓLNA FUNKCJA
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var baseUrl = prefix + 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '4241',
+	'X-Auth-Token': '9f363cc65e90f9994ff68a36893eff0c',
+	'Content-Type': 'application/json',
+};
+//query API data
+fetch(prefix + baseUrl + '/board', { headers: myHeaders })
+	.then(function (resp) {
+		return resp.json();
+	})
+	.then(function (resp) {
+		setupColumns(resp.columns);
+	});
+//set up colums from API data
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.addColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+//set up cards from API data
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
---
+++ js/board.js
@@ -0,0 +1,37 @@
+var board = {
+    name: 'Tablica Kanban',
+    addColumn: function(column) {
+      this.element.appendChild(column.element);
+      initSortable(column.id); 
+    },
+    element: document.querySelector('#board .column-container')
+};
+
+document.querySelector('#board .create-column').addEventListener('click', function() {
+    var name = prompt('Enter a column name');
+    var data = new FormData();
+  
+    data.append('name', name);
+  
+    fetch(baseUrl + '/column', {
+        method: 'POST',
+        headers: myHeaders,
+        body: data,
+      })
+      .then(function(resp) {
+        return resp.json();
+      })
+      .then(function(resp) {
+        var column = new Column(resp.id, name);
+        board.addColumn(column);
+      });
+  });
+	
+function initSortable(id) {
+  	var el = document.getElementById(id);
+  	var sortable = Sortable.create(el, {
+    	group: 'kanban',
+    	sort: true,handle: ".handle",
+        animation: 150
+  	});
+}
