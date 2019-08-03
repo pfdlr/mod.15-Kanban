3c95510bf3c85d5e93263fbe7de632ed1e3ab341
Piotr Fidler <fidler.piotr@gmail.com>
2019-08-03 12:42:55 +0200

    correction dragg card

--- js/card.js
+++ js/card.js
@@ -1,12 +1,11 @@
 // KLASA KANBAN CARD
 function Card(id, name) {
- // var self = this;
-
+ 
   this.id = id;
   this.name = name || "No card name given";
   this.element = generateTemplate("card-template", { description: this.name }, "li");
 
-  this.element.querySelector(".card").addEventListener("click", (event) => {
+  this.element.querySelector(".card").addEventListener("click", event => {
     event.stopPropagation();
 
     if (event.target.classList.contains("btn-delete")) {
@@ -14,13 +13,13 @@ function Card(id, name) {
     }
   });
   //Rename card
-  this.element.querySelector(".card-description").addEventListener("keydown", (event) => {
+  this.element.querySelector(".card-description").addEventListener("keydown", event => {
     var esc = event.which == 27,
       nl = event.which == 13,
       el = event.target,
       input = el.nodeName != "INPUT" && el.nodeName != "TEXTAREA",
       newText = el.innerHTML;
-    var colId = this.parentElement.parentElement.parentElement.id;
+    var colId = this.element.parentElement.id;
     data = {
       name: newText,
       bootcamp_kanban_column_id: colId
@@ -38,28 +37,25 @@ function Card(id, name) {
     }
   });
   //Move card
-  this.element.ondragend = function() {
+  this.element.ondragend = () => {
     data = {
       name: this.name,
       bootcamp_kanban_column_id: targetColId
     };
     this.modifyCard(data);
-  }
+  };
 }
 
 Card.prototype = {
   removeCard: function() {
-    //var self = this;
-
     fetch(baseUrl + "/card/" + this.id, { method: "DELETE", headers: myHeaders })
-      .then((resp) => resp.json())
-      .then((resp) => this.element.parentNode.removeChild(this.element))
-      .catch((error) => console.log(error))
+      .then(resp => resp.json())
+      .then(resp => this.element.parentNode.removeChild(this.element))
+      .catch(error => console.log(error));
   },
   modifyCard: function(data) {
-    //var self = this;
     fetch(baseUrl + "/card/" + this.id, { method: "PUT", headers: myHeaders, body: JSON.stringify(data) })
-      .then((resp) => resp.json())
-      .catch((error) => console.log(error));
+      .then(resp => resp.json())
+      .catch(error => console.log(error));
   }
 };
